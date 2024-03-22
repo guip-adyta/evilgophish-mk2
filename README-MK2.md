f4603b30eb57c6bf079d9755e2e6f5abf199c0ed
guip-adyta <guilherme.pereira@adyta.pt>
2024-03-22 00:39:10 +0000

    Sync fork

---
+++ README-MK2.md
@@ -0,0 +1,22 @@
+# Changes
+
+## Makefile
+- `clean` target
+
+## setup.sh
+`setup.sh` has been provided to automate the needed configurations for you. Once this script is run and you've fed it the right values, you should be ready to get started. Below is the setup help (note that certificate setup is based on `letsencrypt` filenames):
+
+```
+Usage:
+./setup <root domain> <subdomain(s)> <root domain bool> <redirect url> <feed bool> <rid replacement> <blacklist bool> <local deployment bool>
+ - root domain                     - the root domain to be used for the campaign
+ - subdomains                      - a space separated list of evilginx3 subdomains, can be one if only one
+ - root domain bool                - true or false to proxy root domain to evilginx3
+ - redirect url                    - URL to redirect unauthorized Apache requests
+ - feed bool                       - true or false if you plan to use the live feed
+ - rid replacement                 - replace the gophish default "rid" in phishing URLs with this value
+ - blacklist bool                  - true or false to use Apache blacklist
+ - local deployment bool           - true or false to run deployment locally
+Example:
+  ./setup.sh example.com "accounts myaccount" false https://redirect.com/ true user_id false false
+```
--- setup.sh
+++ setup.sh
@@ -26,10 +26,10 @@ function print_info () {
     echo -e "[${script_name}] \x1B[01;34m[*]\x1B[0m $1"
 }
 
-if [[ $# -ne 7 ]]; then
+if [[ $# -ne 8 ]]; then
     print_error "Missing Parameters:"
     print_error "Usage:"
-    print_error './setup <root domain> <subdomain(s)> <root domain bool> <redirect url> <feed bool> <rid replacement> <blacklist bool>'
+    print_error './setup <root domain> <subdomain(s)> <root domain bool> <redirect url> <feed bool> <rid replacement> <blacklist bool> <local deployment bool>'
     print_error " - root domain                     - the root domain to be used for the campaign"
     print_error " - subdomains                      - a space separated list of subdomains to proxy to evilginx3, can be one if only one"
     print_error " - root domain bool                - true or false to proxy root domain to evilginx3"
@@ -37,28 +37,41 @@ if [[ $# -ne 7 ]]; then
     print_error " - feed bool                       - true or false if you plan to use the live feed"
     print_error " - rid replacement                 - replace the gophish default \"rid\" in phishing URLs with this value"
     print_error " - blacklist bool                  - true or false to use Apache blacklist"
+    print_error " - local deployment bool           - true or false to run deployment locally"
     print_error "Example:"
-    print_error '  ./setup.sh example.com "accounts myaccount" false https://redirect.com/ true user_id false'
+    print_error '  ./setup.sh example.com "accounts myaccount" false https://redirect.com/ true user_id false false'
 
     exit 2
 fi
 
 # Set variables from parameters
-root_domain="${1}"
-evilginx3_subs="${2}"
-e_root_bool="${3}"
-redirect_url="${4}"
-feed_bool="${5}"
-rid_replacement="${6}"
+root_domain="${1:-localtest.com}"
+evilginx3_subs="${2:-'sub'}"
+e_root_bool="${3:-false}"
+redirect_url="${4:-https://microsoft.com}"
+feed_bool="${5:-true}"
+rid_replacement="${6:-false}"
 evilginx_dir=$HOME/.evilginx
-bl_bool="${7}"
+bl_bool="${7:-false}"
+local_bool="${8-:true}"
 
 # Get path to certificates
 function get_certs_path () {
-    print_info "Run the command below to generate letsencrypt certificates (will need to create two (2) DNS TXT records):"
-    print_info "letsencrypt|certbot certonly --manual --preferred-challenges=dns --email admin@${root_domain} --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d '*.${root_domain}' -d '${root_domain}'"
-    print_info "Once certificates are generated, enter path to certificates:"
-    read -r certs_path
+    if [[ $(echo "${local_bool}" | grep -ci "true") -gt 0 ]]; then
+        local cur_dir=$(pwd)
+        local cert_file="$cur_dir/fullchain.pem"
+        local key_file="$cur_dir/privkey.pem"
+        openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+            -keyout "$key_file" -out "$cert_file" \
+            -subj "/C=PT/ST=YOS/L=YOL/O=YOO/CN=$root_domain" \
+            -addext "subjectAltName=DNS:$root_domain,DNS:*.$root_domain" 
+        certs_path="$cur_dir"
+    else
+        print_info "Run the command below to generate letsencrypt certificates (will need to create two (2) DNS TXT records):"
+        print_info "letsencrypt|certbot certonly --manual --preferred-challenges=dns --email admin@${root_domain} --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d '*.${root_domain}' -d '${root_domain}'"
+        print_info "Once certificates are generated, enter path to certificates:"
+        read -r certs_path
+    fi
     if [[ ${certs_path: -1} != "/" ]]; then
         certs_path+="/"
     fi
@@ -143,6 +156,8 @@ function setup_evilginx3 () {
     cd ..
     print_good "Configured evilginx3!"
 }
+# phishlets get-hosts o365
+# lures create o365
 
 # Configure and install gophish
 function setup_gophish () {
@@ -156,7 +171,7 @@ function setup_gophish () {
         print_good "Live feed configured! cd into evilfeed then launch binary with ./evilfeed to start!"
     fi
     # Replace rid with user input
-    find . -type f -exec sed -i "s|client_id|${rid_replacement}|g" {} \;
+    find . -type f -exec sed -i "s|user_id|${rid_replacement}|g" {} \;
     cd gophish || exit 1
     go build
     cd ..
@@ -175,3 +190,10 @@ function main () {
 }
 
 main
+
+# ./gophish
+# ./evilfeed
+# sudo ./evilginx3 -g /mnt/c/Users/GuilhermePereira/Desktop/diurnalis/tools/evilgophish-mk2/gophish/gophish.db -p legacy_phishlets -developer -c ../conf/evilginx
+# ---
+# sudo netstat -nltp | grep 53
+# sudo systemctl stop systemd-resolved.service / nslookup google.com / vi /etc/resolv.conf >>> nameserver 1.1.1.1 nameserver 8.8.8.8
